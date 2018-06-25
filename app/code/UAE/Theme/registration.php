702f1291de9173177a45222f79b14ba8151e01aa
Brendan Tull <brendantull@brendan-tull.local>
2018-06-24 22:01:05 -0500

    UAE-564_74 - Add theme module; Set theme via code

---
+++ app/code/UAE/Theme/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @category    ClassyLlama
+ * @copyright   Copyright (c) 2018 Classy Llama
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'UAE_Theme', __DIR__);
