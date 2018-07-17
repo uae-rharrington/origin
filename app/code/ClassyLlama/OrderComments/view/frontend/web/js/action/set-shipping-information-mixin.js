78252855d3058cf7a3a4070600e4a606d3c5bf3e
igori <ivchenkoigor85@gmail.com>
2018-07-17 17:54:05 +0300

    #79: Order Comments

---
+++ app/code/ClassyLlama/OrderComments/view/frontend/web/js/action/set-shipping-information-mixin.js
@@ -0,0 +1,20 @@
+define([
+    'jquery',
+    'mage/utils/wrapper',
+    'Magento_Checkout/js/model/quote'
+], function ($, wrapper,quote) {
+    'use strict';
+
+    return function (setShippingInformationAction) {
+        return wrapper.wrap(setShippingInformationAction, function (originalAction) {
+            var shippingAddress = quote.shippingAddress();
+            if (shippingAddress['extension_attributes'] === undefined) {
+                shippingAddress['extension_attributes'] = {};
+            }
+
+            shippingAddress['extension_attributes']['order_comment'] = shippingAddress.customAttributes['order_comment'];
+            // pass execution to original action ('Magento_Checkout/js/action/set-shipping-information')
+            return originalAction();
+        });
+    };
+});
