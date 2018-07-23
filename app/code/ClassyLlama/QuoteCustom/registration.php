8adb479a9409983bc22ccd36bea695f330888b06
igori <ivchenkoigor85@gmail.com>
2018-07-23 19:51:12 +0300

    #64: Save Order as a Quote - User Story, Acceptance Criteria & Notes - DEV 3 - Remove checkout step

---
+++ app/code/ClassyLlama/QuoteCustom/registration.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Registration config
+ *
+ * @category ClassyLlama
+ * @package ClassyLlama_QuoteCustom
+ * @copyright Copyright (c) 2018 ClassyLlama
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'ClassyLlama_QuoteCustom',
+    __DIR__
+);
