72e4dce2663acbc77e550a0c35733b1ce587a3da
igori <ivchenkoigor85@gmail.com>
2018-08-07 15:26:32 +0300

    commit deploy

--- app/etc/config.php
+++ app/etc/config.php
@@ -245,6 +245,6 @@ return [
         'Xtento_OrderExport' => 1,
         'Xtento_StockImport' => 1,
         'Xtento_TrackingImport' => 1,
-        'ClassyLlama_OrderExport' => 1
+        'UAE_OrderExport' => 1,
     ]
 ];
