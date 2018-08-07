908fbad7b7d93a68ac2632a0c2af4237b48e2f35
igori <ivchenkoigor85@gmail.com>
2018-08-07 13:09:48 +0300

    #79: Order Comments - QA4. For signed-in user the comment for normal order without quote isn't shown on My Account or email, or admin

--- app/code/UAE/OrderComments/view/frontend/web/js/action/set-shipping-information-mixin.js
+++ app/code/UAE/OrderComments/view/frontend/web/js/action/set-shipping-information-mixin.js
@@ -12,22 +12,25 @@ define([
 
     return function (setShippingInformationAction) {
         return wrapper.wrap(setShippingInformationAction, function (originalAction) {
-            var shippingAddress = quote.shippingAddress();
+            var shippingAddress = quote.shippingAddress(),
+            orderComment;
+
             if (shippingAddress['extension_attributes'] === undefined) {
                 shippingAddress['extension_attributes'] = {};
             }
 
             if (shippingAddress.customAttributes !== undefined) {
-                shippingAddress['extension_attributes']['order_comment'] = shippingAddress.customAttributes['order_comment'];
-            } else {
-                var orderComment = $('#onepage-checkout-shipping-method-additional-load')
+                orderComment = shippingAddress.customAttributes['order_comment'];
+            }
+
+            if (orderComment === undefined) {
+                orderComment = $('#onepage-checkout-shipping-method-additional-load')
                     .find("div[name='shippingAddress.custom_attributes.order_comment']")
                     .find('textarea')
                     .val();
-                if (orderComment !== undefined) {
-                    shippingAddress['extension_attributes']['order_comment'] = orderComment;
-                }
             }
+
+            shippingAddress['extension_attributes']['order_comment'] = orderComment ? orderComment : '';
             return originalAction();
         });
     };
