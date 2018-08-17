4058109af273b7e24e561150d1ccbd19b36ee31f
Ben Walley <ben.walley@classyllama.com>
2018-08-17 13:26:38 -0500

    UAE-564_18 - Only show attributes with values on PDP.

---
+++ app/code/UAE/Theme/Block/Catalog/Product/View/Attributes.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @category    ClassyLlama
+ * @copyright   Copyright (c) 2018 Classy Llama
+ */
+
+/**
+ * @inheritdoc
+ */
+namespace UAE\Theme\Block\Catalog\Product\View;
+
+use Magento\Framework\Phrase;
+
+/**
+ * @inheritdoc
+ */
+class Attributes extends \Magento\Catalog\Block\Product\View\Attributes
+{
+    /**
+     * @inheritdoc
+     *
+     * @param array $excludeAttr
+     * @return array
+     */
+    public function getAdditionalData(array $excludeAttr = [])
+    {
+        $data = [];
+        $product = $this->getProduct();
+        $attributes = $product->getAttributes();
+        foreach ($attributes as $attribute) {
+            if ($attribute->getIsVisibleOnFront() && !in_array($attribute->getAttributeCode(), $excludeAttr)) {
+                $value = $attribute->getFrontend()->getValue($product);
+
+                if ($value instanceof Phrase) {
+                    $value = (string)$value;
+                } elseif ($attribute->getFrontendInput() == 'price' && is_string($value)) {
+                    $value = $this->priceCurrency->convertAndFormat($value);
+                }
+
+                if (is_string($value) && strlen($value) && !ctype_space($value) && ($value !== 'No' || $attribute -> getFrontendInput() === 'boolean')) {
+                    $data[$attribute->getAttributeCode()] = [
+                        'label' => __($attribute->getStoreLabel()),
+                        'value' => $value,
+                        'code' => $attribute->getAttributeCode(),
+                    ];
+                }
+            }
+        }
+        return $data;
+    }
+}
