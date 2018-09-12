11c2d0b5c63b55dd897450fae01c8a07f3e32bae
Ryan <rharrington@unitednow.com>
2018-09-12 14:49:25 -0400

    small fix

--- app/code/UAE/NewsLetter/Controller/Manage/Index.php
+++ app/code/UAE/NewsLetter/Controller/Manage/Index.php
@@ -16,10 +16,6 @@ class Index extends \UAE\NewsLetter\Controller\Manage
     public function execute()
     {
         $this->_view->loadLayout();
-
-        if ($block = $this->_view->getLayout()->getBlock('customer_newsletter')) {
-            $block->setRefererUrl($this->_redirect->getRefererUrl());
-        }
         $this->_view->getPage()->getConfig()->getTitle()->set(__('Email Subscription'));
         $this->_view->renderLayout();
     }
