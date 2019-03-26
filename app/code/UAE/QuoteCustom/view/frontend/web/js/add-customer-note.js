ed51cc7e81971bc47d8d0885a61229d43279cb88
Ryan <rharrington@unitednow.com>
2019-03-26 04:11:44 -0400

    Upgrade

--- app/code/UAE/QuoteCustom/view/frontend/web/js/add-customer-note.js
+++ app/code/UAE/QuoteCustom/view/frontend/web/js/add-customer-note.js
@@ -23,7 +23,8 @@ require([
                         + 'of our website to access later.';
                 } else {
                     message = 'To use our instant quote feature please fill out the form below. We will email you a '
-                        + 'quote which is valid for ' + quoteLifetime + ' days.';
+                        + 'quote which is valid for ' + quoteLifetime + ' days. To be able to view the quote from the "my account" section '
+                        + 'of our website to access later, be sure to log in now or create an account after you submit the quote.';
                 }
 
                 checkoutShippingStepElement.prepend(
